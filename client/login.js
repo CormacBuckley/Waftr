83f16d4e188e2f329bb7a8d58aec1f3173b34462
Naoise Callery <naoisecallery@gmail.com>
2017-01-12 17:11:16 +0000

    Html files uploaded

---
+++ client/login.js
@@ -0,0 +1,22 @@
+function loginFunction(){
+	
+}
+function registerFunction(){
+	
+}
+
+function login(showhide){
+if(showhide == "show"){
+    document.getElementById('popupbox1').style.visibility="visible";
+}else if(showhide == "hide"){
+    document.getElementById('popupbox1').style.visibility="hidden"; 
+}
+}
+
+function register(showhide){
+if(showhide == "show"){
+    document.getElementById('popupbox2').style.visibility="visible";
+}else if(showhide == "hide"){
+    document.getElementById('popupbox2').style.visibility="hidden"; 
+}
+}
