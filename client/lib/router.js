31c6f97c8c5a045e06af5b539e3783e73d74598c
eamondunne <eamon.dunne@gmail.com>
2017-03-10 01:05:10 +0000

    Updated Router

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,7 +1,38 @@
-Router.route('/', {
-template: 'home'
+Router.configure({
+  loadingTemplate: 'spinner',
+  notFoundTemplate: 'notFound',
 });
-Router.route('/news');
+
+Router.route('/', {
+  name: 'root',
+  controller: 'MainPageController',
+});
+
+Router.route('/new', {
+  name: 'newPost',
+  controller: 'BaseController',
+});
+
+Router.route('/:_id', {
+  name: 'singlePost',
+  controller: 'SinglePostController',
+});
+
+Router.onBeforeAction(function () {
+  if (!Meteor.user()) {
+    if (Meteor.loggingIn()) {
+      this.render(this.loadingTemplate);
+    } else {
+      this.render('accessDenied');
+    }
+  } else {
+    this.next();
+  }
+},
+
+{
+  only: 'newPost',
+});
 Router.route('/collegelife');
 Router.route('/events');
 Router.route('/talk');
