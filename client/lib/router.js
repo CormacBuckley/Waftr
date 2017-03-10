f3bbbed6ae0c8e9c5d86db6f6fc6e7465603b0ae
eamondunne <eamon.dunne@gmail.com>
2017-03-10 01:13:35 +0000

    Revert "Updated Router"
    This reverts commit 31c6f97c8c5a045e06af5b539e3783e73d74598c.

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,38 +1,7 @@
-Router.configure({
-  loadingTemplate: 'spinner',
-  notFoundTemplate: 'notFound',
-});
-
 Router.route('/', {
-  name: 'root',
-  controller: 'MainPageController',
+template: 'home'
 });
-
-Router.route('/new', {
-  name: 'newPost',
-  controller: 'BaseController',
-});
-
-Router.route('/:_id', {
-  name: 'singlePost',
-  controller: 'SinglePostController',
-});
-
-Router.onBeforeAction(function () {
-  if (!Meteor.user()) {
-    if (Meteor.loggingIn()) {
-      this.render(this.loadingTemplate);
-    } else {
-      this.render('accessDenied');
-    }
-  } else {
-    this.next();
-  }
-},
-
-{
-  only: 'newPost',
-});
+Router.route('/news');
 Router.route('/collegelife');
 Router.route('/events');
 Router.route('/talk');
