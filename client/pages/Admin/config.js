8d1f196c9749e543521a923650367228b9bf27f0
EamonCullen <eamonc123@gmail.com>
2017-03-23 11:43:30 +0000

    Fixed top-nav padding (again) and removed duplicate CSS

--- client/pages/Admin/config.js
+++ client/pages/Admin/config.js
@@ -1,5 +1,5 @@
 AdminConfig = {
-adminEmails: ['eamondunne@gmail.com','cbuckley97@gmail.com','ed123@hotmail.com'],
+adminEmails: ['eamondunne@gmail.com','cbuckley97@gmail.com','ed123@hotmail.com', 'eamonc123@gmail.com'],
 collections:
 {
 //News: {}
