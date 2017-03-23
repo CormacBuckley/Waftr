035a6c41901bfee4c6923952626c7cc3b589a4ac
Cormac Buckley <cbuckley97@gmail.com>
2017-03-23 11:45:45 +0000

    Added Schema to posts collection

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,43 @@
 Posts = new Mongo.Collection('posts');
+
+Posts.allow({
+  insert : function(userId, doc){
+    return !!userId;
+  }
+})
+
+PostSchema = new SimpleSchema({
+  body:{
+    type: String,
+    label: "Post"
+  },
+  _id: {
+		type: String,
+		optional: true,
+		autoform: {
+			omit: true
+		}
+	},
+  author: {
+		type: String,
+		label: "Author",
+		autoValue: function(){
+			return Meteor.userId()
+		},
+		autoform: {
+			type: "hidden"
+		}
+	},
+  createdAt: {
+		type: Date,
+		label: "Created At",
+		autoValue: function() {
+			return new Date()
+		},
+		autoform: {
+			type: "hidden"
+		}
+	}
+});
+
+Posts.attachSchema(PostSchema);
